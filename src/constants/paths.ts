dbb4bc11da26d2c2290acd132cda4ec85a376347
Armend Krasniqi <armendkras@Armends-MacBook-Pro-2.local>
2023-12-14 22:26:06 +0100

    i added example as comments of how to use reactQuery hooks

---
+++ src/constants/paths.ts
@@ -0,0 +1,3 @@
+export const ENDPOINTS = {
+    EXAMPLE: "entries?category=health&https=true"
+  }
--- src/screens/App/Home/index.tsx
+++ src/screens/App/Home/index.tsx
@@ -4,8 +4,29 @@ import Header from "./HomeComponents/Header";
 import CustomSlider from "./HomeComponents/Slider";
 import { Colors } from "../../../constants/colors";
 import Content from "./HomeComponents/Content";
+// import { ENDPOINTS } from "../../../constants/paths";
+// import useGetRequest from "../../../hooks/reactQuery/useGetRequest";
+// import { usePostMutation } from '../../../hooks/reactQuery/useMutation';
 
 const HomeScreen = () => {
+  // const payload = {
+  //   paylod: "example",
+  // };
+
+  // Example for get request
+
+  // const { data, error } = useGetRequest(ENDPOINTS.EXAMPLE);
+  // console.log("data", data?.data);
+  //Example for mutations
+  //Same with put and delete
+  // const postMutation = usePostMutation<unknown>(ENDPOINTS.EXAMPLE,
+  //   //On Success
+  //   (data:unknown)=>{console.log("Posted !! Success",data)},
+  //   //On Error
+  //   (error:unknown)=>{console.log('Posted !! Error',error)}
+  // );
+  // postMutation.mutate(payload); use on your function
+
   return (
     <View style={{ backgroundColor: Colors.white, flex: 1 }}>
       <Header />
